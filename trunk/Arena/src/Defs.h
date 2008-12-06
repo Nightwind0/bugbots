ef8968c7a5dd0598240895cc4ddfa3c46fe44b9d
nightwind <nightwind@18e9229b-32c3-44d5-97e8-11808f8a6557>
2008-12-06 05:04:32 +0000

    Integrate quadtree in as test.

--- trunk/Arena/src/Defs.h
+++ trunk/Arena/src/Defs.h
@@ -3,7 +3,7 @@
 
 
 const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_HEIGHT = 800;
 const int SCREEN_BPP = 32;
 
 
--- trunk/Arena/src/Main.cpp
+++ trunk/Arena/src/Main.cpp
@@ -1,12 +1,20 @@
 #include "SDL/SDL.h"
 #include "Defs.h"
+#include "Quadtree/Node.hxx"
+#include "Quadtree/Geometry.hxx"
+
+
+using Quadtree::Node;
+
+typedef Quadtree::Node<int,4,int> QTNode;
+typedef Quadtree::Geometry::Vector<int> QTVector;
 
 int main( int argc, char* args[] ) { 
 	//Start SDL 
 	if(SDL_Init( SDL_INIT_EVERYTHING ) == -1){
 		return 1;
 	}
-
+	QTNode tree(NULL,QTNode::Square(QTVector(0,0),800));
 	SDL_Surface *screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE ); 
 	//Quit SDL 
 	SDL_Quit(); 
