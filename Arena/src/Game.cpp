961a53fb19f0de22c5a460f7d7b03c3a61a06a5a
nightwind <nightwind@18e9229b-32c3-44d5-97e8-11808f8a6557>
2011-01-22 06:40:49 +0000

    Starting platform for Arena

--- Arena/src/Game.cpp
+++ Arena/src/Game.cpp
@@ -24,19 +24,8 @@ void Game::OnRender(){
   // Ask SDL for the time in milliseconds
   int tick = SDL_GetTicks();
 
-  // Declare a couple of variables
-  int i, j, yofs, ofs;
-
   // Draw to screen
-  yofs = 0;
-  for (i = 0; i < SCREEN_HEIGHT; i++)
-  {
-    for (j = 0, ofs = yofs; j < SCREEN_WIDTH; j++, ofs++)
-    {
-      ((unsigned int*)m_screen->pixels)[ofs] = i * i + j * j + tick;
-    }
-    yofs += m_screen->pitch / 4;
-  }
+
 
   // Unlock if needed
   if (SDL_MUSTLOCK(m_screen)) 
@@ -54,6 +43,8 @@ void Game::OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode){
 }
 
 void Game::OnKeyUp(SDLKey sym, SDLMod mod, Uint16 unicode){
+  if(sym == SDLK_ESCAPE)
+    OnExit();
 }
 
 void Game::OnMouseFocus(){
